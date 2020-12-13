5d2e08f48059fce94ceba5e874ee18c2cb2c63d2
Tanay J <33786522+Tanay-J@users.noreply.github.com>
2020-12-13 10:45:44 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  var [meaning, setMeaning] = useState("");
+  var [inputEmoji, setInputEmoji] = useState("");
+  var emojiDb = {
+    "😀": "Grinning Face",
+    "☺️": " Smiling Face",
+    "😉": " Winking Face",
+    "🤩": "Star-Struck",
+    "🤗": "Hugging Face",
+    "🤭": "Face with Hand Over Mouth",
+    "🤫": "Shushing Face",
+    "🤔": "Thinking Face",
+    "🤐": "Zipper-Mouth Face",
+    "🤨": "Face with Raised Eyebrow",
+    "😐": "Neutral Face",
+    "😑": "Expressionless Face",
+    "😶": "Face Without Mouth",
+    "😏": "Smirking Face",
+    "😒": "Unamused Face",
+    "🙄": "Face with Rolling Eyes",
+    "😬": "Grimacing Face",
+    "🤥": "Lying Face",
+    "😌": "Relieved Face",
+    "😔": "Pensive Face",
+    "😪": "Sleepy Face",
+    "🤤": "Drooling Face",
+    "😷": "Face with Medical Mask",
+    "🤒": "Face with Thermometer",
+    "🤕": "Face with Head-Bandage",
+    "🤢": "Nauseated Face",
+    "🤮": "Face Vomiting",
+    "🤧": "Sneezing Face",
+    "🥵": "Hot Face",
+    "🥶": "Cold Face",
+    "🥴": "Woozy Face",
+    "😵": "Dizzy Face",
+    "🤯": "Exploding Head",
+    "🤠": "Cowboy Hat Face",
+    "🥳": "Partying Face"
+  };
+  var emojiArray = Object.keys(emojiDb);
+
+  function changeHandler(event) {
+    inputEmoji = event.target.value;
+    setInputEmoji(inputEmoji);
+    if (inputEmoji in emojiDb) {
+      meaning = emojiDb[inputEmoji];
+    } else {
+      meaning = "Can't find the emoji";
+    }
+    setMeaning(meaning);
+  }
+
+  function emojiClickHandler(emoji) {
+    meaning = emojiDb[emoji];
+    setMeaning(meaning);
+    setInputEmoji(emoji);
+  }
+  return (
+    <div className="App">
+      <h1>Know Your Emoji</h1>
+
+      <div class="meaning">
+        {" "}
+        {inputEmoji} : {meaning}{" "}
+      </div>
+      <input
+        class="inputArea"
+        onChange={changeHandler}
+        placeholder="Search emoji"
+      ></input>
+      <h3>Click on the emoji to search</h3>
+      <p>
+        {emojiArray.map(function (emoji) {
+          return (
+            <span
+              class="emojiList"
+              onClick={function () {
+                return emojiClickHandler(emoji);
+              }}
+            >
+              {emoji}
+            </span>
+          );
+        })}
+      </p>
+    </div>
+  );
+}
